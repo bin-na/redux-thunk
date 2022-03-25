04296d299fe1911e8ebc124f3b6bcd69196a2afd
hoangtrungnamus <nampanama0101@gmail.com>
2022-03-25 09:34:25 +0700

    update login page

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,28 +1,22 @@
-import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
-import { history } from '../helpers';
-import { Home, Same } from '../Pages';
-import Login from '../pages/login';
-import Register from '../pages/register';
-import './app.css';
+import { history } from "../helpers";
+import { Home, Same } from "../Pages";
+import Login from "../pages/login";
+import Register from "../pages/register";
+import "./app.css";
 
 function App() {
   return (
-    <div className='jumbotron'>
-      <div className='container'>
-        <div className='col-md-8 offset-md-2'>
-          <BrowserRouter history={history}>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/same' element={<Same />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/login' element={<Login />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter history={history}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/same" element={<Same />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,79 +1,85 @@
-import { Form, Input, Button, Checkbox } from 'antd';
-import React from 'react';
-const Login = () => {
+import React from "react";
+import { Form, Input, Button, Checkbox, Row, Col, Typography } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+const { Text } = Typography;
+export default function Login() {
   const onFinish = (values) => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+    console.log("Received values of form: ", values);
   };
 
   return (
-    <Form
-      name='basic'
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete='off'
-    >
-      <Form.Item
-        label='Tên đăng nhập'
-        name='username'
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập thông tin!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <div className="my-container">
+      <Col xs={12} sm={8}>
+        <Form
+          size="large"
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
+          >
+            <Input
+              size="large"
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox style={{ color: "blue", fontWeight: "bold" }}>
+                Remember me
+              </Checkbox>
+            </Form.Item>
 
-      <Form.Item
-        label='Mật khẩu'
-        name='password'
-        rules={[
-          {
-            required: true,
-            message: 'Vui lòng nhập thông tin!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
 
-      <Form.Item
-        name='remember'
-        valuePropName='checked'
-        wrapperCol={{
-          offset: 6,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 6,
-          span: 16,
-        }}
-      >
-        <Button type='primary' htmlType='submit'>
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="register-form-button"
+            >
+              <Link to="/register">Register</Link>
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </div>
   );
-};
-
-export default Login;
+}
